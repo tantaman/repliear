163a4585354620a7d90b158560ce2a1b697c7317
Matt <1009003+tantaman@users.noreply.github.com>
2023-12-05 15:44:35 -0500

    Revert "sync lock"
    This reverts commit f1521b5f5c9557ee83d3ae05ecbdf96d5e533262.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,6 @@ import {mutators} from './model/mutators';
 import {Replicache} from 'replicache';
 import {UndoManager} from '@rocicorp/undo';
 import App from './app';
-import {lock} from './util/sync-lock';
 
 async function init() {
   // See https://doc.replicache.dev/licensing for how to get a license key.
@@ -13,34 +12,11 @@ async function init() {
     throw new Error('Missing VITE_REPLICACHE_LICENSE_KEY');
   }
 
-  const syncLock = lock('pull');
   const r = new Replicache({
     name: 'anon',
     licenseKey,
     mutators,
     logLevel: 'debug',
-    puller: async requestBody => {
-      if (!syncLock.held) {
-        return {
-          httpRequestInfo: {
-            errorMessage: 'Tab does not hold the pull lock',
-            httpStatusCode: 400,
-          },
-        };
-      }
-      const res = await fetch('/api/replicache/pull', {
-        method: 'POST',
-        headers: {'content-type': 'application/json'},
-        body: JSON.stringify(requestBody),
-      });
-      return {
-        response: await res.json(),
-        httpRequestInfo: {
-          errorMessage: !res.ok ? res.statusText : '',
-          httpStatusCode: res.status,
-        },
-      };
-    },
     pushURL: `/api/replicache/push`,
     pullURL: `/api/replicache/pull`,
   });
@@ -57,14 +33,6 @@ async function init() {
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Root />,
   );
-
-  // Issue a pull whenever we become leader.
-  syncLock.onStatusChange(async held => {
-    if (held) {
-      console.log('status change?');
-      await r.pull();
-    }
-  });
 }
 
 await init();
--- client/src/util/sync-lock.ts
+++ client/src/util/sync-lock.ts
@@ -1,152 +1,56 @@
-export type Options = {
-  channelFactory: (name: string) => {
-    onmessage: ((event: MessageEvent<number>) => void) | null;
-    postMessage: (number: number) => void;
-    close: () => void;
-  };
-  intervalMs: number;
-  nextNumber: () => number;
-  visible: boolean;
-};
-
-const defaultOptions: Options = {
-  channelFactory: name => {
-    return new BroadcastChannel(name);
-  },
-  intervalMs: 100,
-  nextNumber: () => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
-  // hidden docs have their timers slowed down
-  // so we'll just take them out of the equation
-  // This isn't exactly ideal if someone wants to load data in the background.
-  get visible() {
-    return !document.hidden;
-  },
-};
+const held = new Set<string>();
 
 /**
- * Coordinates access to a named resource between tabs or workers (aka actors).
+ * Acquires a lock for the lifetime of a tab.
+ * When the tab is closed, crashes,
+ * and presumably put to sleep?, <-- research this one
+ * the lock is released.
  *
- * Each actor is assigned a number.
- * The actor with the highest number obtains the named resource.
+ * Why this lock thing?
+ * The backend `pull` API currently serializes on the client group.
+ * If we have two tabs going full blast on sync we get serialization errors very frequently.
+ * We could:
+ * a. fix the backend
+ * b. coordinate sync between tabs
  *
- * Actors with lesser numbers may not use the resource.
- *
- * Actors broadcast their numbers to each other every INTERVAL / 4 milliseconds.
- *
- * Actors check if they are the greatest every INTERVAL milliseconds.
- * If they are, they will acquire the resource.
- * If they are not, they may not use the resource and will release the resources if the previously had it.
- *
- * Actors that have not broadcasted their number in INTERVAL milliseconds are considered to have left.
- *
- * Listen to `onStatusChange` to know when the current actor receives or loses the resource.
+ * Note: tabs can be forked and get new client groups.. so we'd need to update `acquire` to deal with that case.
  *
  * @param name
  * @returns
  */
-export function lock(name: string, options: Options = defaultOptions) {
-  return new SyncLock(name, options);
-}
+export function acquire(name: string) {
+  const ret: {
+    held: boolean;
+    onAcquired?: () => void;
+  } = {
+    held: false,
+  };
 
-type Time = number;
-type Value = number;
+  if (held.has(name)) {
+    ret.held = true;
+    return ret;
+  }
 
-class SyncLock {
-  #number;
-  #channel;
-  #acquired = false;
-  readonly #peers: Map<Value, Time> = new Map();
-  readonly #listeners = new Set<(held: boolean) => void>();
-  readonly #options: Options;
-  readonly #destroyers: (() => void)[] = [];
+  const p = new Promise(() => {
+    // Do nothing.
+  });
 
-  constructor(public name: string, options: Options) {
-    this.#number = options.nextNumber();
-    this.#channel = options.channelFactory(`sync-lock-${name}`);
-    this.#options = options;
-
-    this.#channel.onmessage = event => {
-      const number = event.data;
-      const now = Date.now();
-      this.#peers.set(number, now);
-      if (number > this.#number && this.#acquired) {
-        this.#acquired = false;
-        this.#notify();
-      }
-    };
-
-    const expireAndBroadcastHandle = setInterval(() => {
-      const now = Date.now();
-      for (const [number, time] of this.#peers.entries()) {
-        if (now - time >= options.intervalMs) {
-          this.#peers.delete(number);
+  navigator.locks
+    .request(
+      name,
+      // Lock is acquired.
+      _lock => {
+        held.add(name);
+        ret.held = true;
+        if (ret.onAcquired) {
+          ret.onAcquired();
         }
-      }
-      if (options.visible) {
-        this.#channel.postMessage(this.#number);
-      }
-    }, options.intervalMs / 4);
-    const acquireHandle = setInterval(() => {
-      this.#maybeAcquire();
-    }, options.intervalMs);
+        return p;
+      },
+    )
+    .catch(e => {
+      console.error(e);
+    });
 
-    this.#destroyers.push(() => clearInterval(expireAndBroadcastHandle));
-    this.#destroyers.push(() => clearInterval(acquireHandle));
-    this.#destroyers.push(() => this.#channel.close());
-  }
-
-  onStatusChange(cb: (held: boolean) => void) {
-    this.#listeners.add(cb);
-    return () => this.#listeners.delete(cb);
-  }
-
-  get held() {
-    return this.#acquired;
-  }
-
-  #maybeAcquire() {
-    if (!this.#options.visible && this.#acquired) {
-      this.#acquired = false;
-      this.#notify();
-      return;
-    }
-
-    if (!this.#options.visible) {
-      // Don't acquire if we're not visible
-      return;
-    }
-
-    let hasMax = true;
-    for (const n of this.#peers.keys()) {
-      if (this.#number < n) {
-        hasMax = false;
-      }
-    }
-
-    if (hasMax) {
-      if (!this.#acquired) {
-        this.#acquired = true;
-        this.#notify();
-      }
-    } else {
-      if (this.#acquired) {
-        this.#acquired = false;
-        this.#notify();
-      }
-    }
-  }
-
-  #notify() {
-    for (const listener of this.#listeners) {
-      listener(this.#acquired);
-    }
-  }
-
-  destroy() {
-    for (const destroyer of this.#destroyers) {
-      destroyer();
-    }
-  }
+  return ret;
 }
-
-export type {SyncLock};
